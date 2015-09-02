7e5994983c3b166f852722e86e77e58c1ffead0d
Ztawh <amandahwatz@gmail.com>
2015-09-02 17:43:54 +0200

    It works! Got some cleaning to do..
    Need to add something to check if letter_in is a char.

--- hangman.rb
+++ hangman.rb
@@ -8,47 +8,102 @@ letters = word_arr.count
 
 puts "I have a word for you. It's #{letters} letters long. Guess what it is by typing it one letter at a time."
 
-#puts letters
-
 letter_in_arr = []
 letters.times do
-	letter_in_arr += ["_ "]
+	letter_in_arr += ["_"]
 end
 
 letter_in_arr.each do |element|
-	print element
+	print element + " "
 end
 
-puts "\n"
+puts "\n \n"
 
-letter_in = gets.chomp
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-def fail
+#letter_in = gets.chomp
+#letter_in = letter_in.downcase
+
+def fail()
 	puts "GAME OVER!!"
 end
 
-def letter_check(letter_in, word_arr, letter_in_arr)
-	tries_left = 0
-	#while tries_left != 9
-		if word_arr.include?(letter_in)
-			letter_in_arr[word_arr.index(letter_in)] = letter_in
-	
-		#if word_arr.include?(letter_in)==false
-		else
-			tries_left += 1
-		end
-	#end
-
-	if tries_left == 8
-		fail()
-	end
-
-
-	letter_in_arr.each do |element|
-		print element
-	end
-	puts "\n"
+def win()
+	puts "Congratulations, you got the word!!"
 end
-letter_check(letter_in, word_arr, letter_in_arr)
 
+#def letter_check(word_arr, letter_in_arr)
+	tries_left = 8
+
+	puts "Available words: "
+		alphabet.each do |element|
+			print element + " "
+		end
+		puts "\n"
+
+		guessed = []
+
+
+	until tries_left == -1
+
+		if letter_in_arr.sort == word_arr.sort
+			win()
+			break
+		end
+
+	
+
+		letter_in = gets.chomp.downcase
+		puts "\n"
+
+		
+		if alphabet.include?(letter_in) && word_arr.include?(letter_in) == false
+			tries_left -= 1
+			puts "Nope! That's not it. You have #{tries_left} tries left."
+		end
+		
+		if guessed.include?(letter_in)
+			puts "You have already tried that letter. Please type a letter from the available ones above."
+		end
+
+		puts "\n"
+
+		guessed << letter_in
+		
+		if alphabet.include?(letter_in)
+			alphabet.delete(letter_in)
+			puts "Available words: "
+			alphabet.each do |element|
+				print element + " "
+			end
+			puts "\n"
+			
+		end
+
+		#if word_arr.include?(letter_in)
+		#	letter_in_arr[word_arr.index(letter_in)] = letter_in
+		#end
+
+		word_arr.each_with_index do |element, index|
+			if word_arr[index] == letter_in
+				letter_in_arr[index] = element
+			end
+		end
+
+		letter_in_arr.each do |element|
+			print element + " "
+		end
+		puts "\n \n"
+
+		if tries_left == 0
+			fail()
+			break
+		end
+
+	end
+
+
+	
+#end
+#letter_check(word_arr, letter_in_arr)
 
